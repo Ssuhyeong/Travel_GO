b552d5e94fe91086e6e8239068742f4235db9201
최경인 <chldudw@naver.com>
2023-05-08 00:26:56 +0900

    feat: attraction 검색 동적쿼리
    attraction 동적쿼리 검색을 위하여 repository 파일 수정

--- backend/src/main/java/com/ssafy/trip/repository/AttractionRepository.java
+++ backend/src/main/java/com/ssafy/trip/repository/AttractionRepository.java
@@ -2,10 +2,9 @@ package com.ssafy.trip.repository;
 
 import com.ssafy.trip.Entity.Attraction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AttractionRepository extends JpaRepository<Attraction,Integer>, JpaSpecificationExecutor<Attraction> {
+public interface AttractionRepository extends JpaRepository<Attraction,Integer>, AttractionRepositoryCustom {
 
 }
