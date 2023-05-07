da8a4c286c13aaee26e4ab0135a9c6c1f80b5a1b
최경인 <chldudw@naver.com>
2023-05-08 00:18:32 +0900

    feat: Qfile 생성
    Querydsl Qfile 생성

---
+++ backend/src/main/generated/com/ssafy/trip/Entity/QAttraction.java
@@ -0,0 +1,53 @@
+package com.ssafy.trip.Entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QAttraction is a Querydsl query type for Attraction
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QAttraction extends EntityPathBase<Attraction> {
+
+    private static final long serialVersionUID = 210529968L;
+
+    public static final QAttraction attraction = new QAttraction("attraction");
+
+    public final StringPath addr1 = createString("addr1");
+
+    public final NumberPath<Integer> content_id = createNumber("content_id", Integer.class);
+
+    public final NumberPath<Integer> content_type_id = createNumber("content_type_id", Integer.class);
+
+    public final StringPath first_image = createString("first_image");
+
+    public final StringPath latitude = createString("latitude");
+
+    public final StringPath longitude = createString("longitude");
+
+    public final StringPath overview = createString("overview");
+
+    public final NumberPath<Integer> sido_code = createNumber("sido_code", Integer.class);
+
+    public final StringPath title = createString("title");
+
+    public QAttraction(String variable) {
+        super(Attraction.class, forVariable(variable));
+    }
+
+    public QAttraction(Path<? extends Attraction> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QAttraction(PathMetadata metadata) {
+        super(Attraction.class, metadata);
+    }
+
+}
+
---
+++ backend/src/main/generated/com/ssafy/trip/Entity/QBoard.java
@@ -0,0 +1,47 @@
+package com.ssafy.trip.Entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QBoard is a Querydsl query type for Board
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QBoard extends EntityPathBase<Board> {
+
+    private static final long serialVersionUID = 1579464189L;
+
+    public static final QBoard board = new QBoard("board");
+
+    public final NumberPath<Integer> articleNo = createNumber("articleNo", Integer.class);
+
+    public final StringPath content = createString("content");
+
+    public final NumberPath<Integer> hit = createNumber("hit", Integer.class);
+
+    public final StringPath registerTime = createString("registerTime");
+
+    public final StringPath subject = createString("subject");
+
+    public final StringPath userId = createString("userId");
+
+    public QBoard(String variable) {
+        super(Board.class, forVariable(variable));
+    }
+
+    public QBoard(Path<? extends Board> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QBoard(PathMetadata metadata) {
+        super(Board.class, metadata);
+    }
+
+}
+
