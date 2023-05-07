1e092ca9850586e2e04d211bb822d7845dcd6c99
최경인 <chldudw@naver.com>
2023-05-08 00:19:12 +0900

    feat: QuerydslConfig
    Querydsl 사용을 위한 configuration 파일 작성

---
+++ backend/src/main/java/com/ssafy/trip/configuration/QuerydslConfig.java
@@ -0,0 +1,21 @@
+package com.ssafy.trip.configuration;
+
+import com.querydsl.core.annotations.Config;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+public class QuerydslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
