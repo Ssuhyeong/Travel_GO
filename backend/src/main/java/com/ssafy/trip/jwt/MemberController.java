2153c1ae333bca36240de923e61acc4938cd9d19
최경인 <chldudw@naver.com>
2023-05-18 10:07:35 +0900

    feat: spring security + jwt spring security + jwt 관련 file

--- backend/src/main/java/com/ssafy/trip/jwt/MemberController.java
+++ backend/src/main/java/com/ssafy/trip/jwt/MemberController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,6 +16,7 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @CrossOrigin
     @PostMapping("/sign-up")
     public ResponseEntity<?> userSignUp(@RequestBody UserSignDto userSignDto) throws Exception {
         memberService.signUp(userSignDto);
